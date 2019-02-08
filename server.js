5be64e2a16724364f82b00f9f47216f872abeacb
dubem <dubemokam@gmail.com>
2019-02-08 18:12:25 -0500

    added heroku config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const plaid = require("./routes/api/plaid");
@@ -37,6 +38,17 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("api/plaid", plaid);
+
+//Serve static content// for deployment to heroku
+if(process.env.NODE_ENV === 'production'){
+
+  //static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
